247feed6c852ed6ffbf597fc0a6d44466edcfe8d
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2021-11-05 22:56:44 +0100

    strapi testing

---
+++ pages/strapi.js
@@ -0,0 +1,78 @@
+import Head from "next/head";
+import Banner from "../components/apis/banner";
+import { GlobalStyles } from "../styles/global";
+import { ThemeProvider } from "styled-components";
+import { useTheme } from "../hooks/useTheme";
+import { lightTheme, darkTheme } from "../styles/theme";
+
+export default function Home({ articles, error }) {
+  const [theme, toggleTheme, componentMounted] = useTheme();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />;
+  }
+
+  if (error) {
+    return <div>An error occured: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyles />
+        <Head>
+          <title>Dot Directory</title>
+          <meta
+            name="description"
+            content="Personal web directory for data, APIs, and design"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Banner toggleTheme={toggleTheme} theme={theme} />
+        <div>
+          <ul>
+            {articles.map((article) => (
+              <li key={article.id}>{article.title}</li>
+            ))}
+          </ul>
+        </div>
+
+        {/* <Footer theme={theme} /> */}
+      </ThemeProvider>
+    </>
+  );
+}
+
+Home.getInitialProps = async (ctx) => {
+  try {
+    // Parses the JSON returned by a network request
+    const parseJSON = (resp) => (resp.json ? resp.json() : resp);
+    // Checks if a network request came back fine, and throws an error if not
+    const checkStatus = (resp) => {
+      if (resp.status >= 200 && resp.status < 300) {
+        return resp;
+      }
+
+      return parseJSON(resp).then((resp) => {
+        throw resp;
+      });
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const articles = await fetch("http://localhost:1337/articles", {
+      method: "GET",
+      headers,
+    })
+      .then(checkStatus)
+      .then(parseJSON);
+
+    return { articles };
+  } catch (error) {
+    return { error };
+  }
+};
