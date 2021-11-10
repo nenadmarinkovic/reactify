5a1bed9d8b2dbfe78dda5d42ecd0b0da3a8c2b66
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2021-11-10 22:23:27 +0100

    footer layout

--- styles/components/footer.js
+++ styles/components/footer.js
@@ -2,6 +2,22 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
   background: ${({ theme }) => theme.bannerBackground};
-  height: 200px;
+  padding: 50px 0 30px 0;
   transition: all 0.25s linear;
+  text-align: center;
+`;
+
+export const Copyright = styled.div`
+  color: ${({ theme }) => theme.mainText};
+  line-height: 1.8;
+
+  a {
+    margin-left: 5px;
+    color: ${({ theme }) => theme.mainText};
+    transition: all 0.25s linear;
+    
+    :hover {
+      color: ${({ theme }) => theme.helperText};
+    }
+  }
 `;
