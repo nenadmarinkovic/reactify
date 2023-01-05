890b25e2b68c7ef84a43ab6aba4ba01b4a735601
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-01-05 17:06:32 +0100

    style update

--- styles/components/highlighter.js
+++ styles/components/highlighter.js
@@ -1,36 +1,36 @@
-
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-font-family: sans-serif;
-text-align: center;
+  font-family: sans-serif;
+  text-align: center;
 `;
 
 export const Pre = styled.pre`
-border-radius: 10px;
-transition: all 0.25s linear;
-background: ${({ theme }) => theme.itemBackground} !important;
-text-align: left;
-padding: 1rem 1.5rem;
+  border-radius: 10px;
+  transition: all 0.25s linear;
+  background: ${({ theme }) => theme.itemBackground} !important;
+  text-align: left;
+  padding: 1rem 1.5rem;
+  overflow: auto;
 
-& .token-line {
-  line-height: 1.3em;
-  height: 1.3em;
-}
+  & .token-line {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
 `;
 
 export const Line = styled.div`
-display: table-row;
+  display: table-row;
 `;
 
 export const LineNo = styled.span`
-display: table-cell;
-text-align: right;
-padding-right: 1em;
-user-select: none;
-opacity: 0.5;
+  display: table-cell;
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
 `;
 
 export const LineContent = styled.span`
-display: table-cell;
+  display: table-cell;
 `;
