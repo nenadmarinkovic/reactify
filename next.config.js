cba324b3d4c2bdf69388ea6eb12651dd8f8c6674
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-03-24 22:10:57 +0100

    fix layout

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+
   images: {
     domains: [
       "www.datocms-assets.com",
       "i.scdn.co",
       "cdn.sanity.io",
-      "images.ctfassets.net"
+      "images.ctfassets.net",
     ],
   },
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
-import Document from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document from "next/document";
+
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+
     const originalRenderPage = ctx.renderPage;
 
     try {
@@ -14,14 +16,11 @@ export default class MyDocument extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
